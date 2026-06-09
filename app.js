8a09c208d8c56a70eecf6a5bfec004750cc704d5
Shegmanie <A01614712@tec.mx>
2026-06-09 11:46:38 -0600

    typos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const router = require('./outes/index');
+const router = require('./routes/index');
 
 const app = express();
 const PORT = 3002;
